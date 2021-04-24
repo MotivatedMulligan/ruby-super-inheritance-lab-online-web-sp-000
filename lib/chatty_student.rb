8b4b1ac2e91d5b716d0aeaea5202169ebba4debf
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-24 23:39:25 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,9 +1,8 @@
 class ChattyStudent < Student
 
-def hello(*)
+def hello#(*)
   super
-
-puts "How are you doing today?
+  puts "How are you doing today?
   I'm okay, but I'm kind of tired.
   Did you watch The Walking Dead last night?
   You didn't?! Oh man, it was so crazy!

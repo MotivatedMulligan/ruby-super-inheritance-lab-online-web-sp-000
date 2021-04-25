abc33e8e07fef51771be5403b27869df10626b19
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-25 00:08:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,8 @@
 class Student
-  def hello
-    puts "Hey there! I'm so excited to learn stuff."
+  def hello(string=nil)
+    puts "Hey there! I'm so excited to learn stuff.#{string}"
   end
   def raise_hand
     puts "Pick me!"
   end
 end
-#Hey there! I'm so excited to learn stuff.
